9cc16182ef6cb6afe555ac2b2e2ab3a922c0e1dd
consent <c0nsent@proton.me>
2025-10-08 15:47:27 +0300

    Минорные фиксы для повышения производительности

--- Темы 1-2/Задачи/A.cxx
+++ Темы 1-2/Задачи/A.cxx
@@ -1,12 +1,31 @@
 #include <algorithm>
+#include <chrono>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
-#include <string>
-#include <string_view>
 #include <vector>
 
-constexpr bool c_debug{ true };
+class Benchmark
+{
+public:
+
+	Benchmark()
+		: m_begin{std::chrono::high_resolution_clock::now()} {}
+
+	~Benchmark()
+	{
+		const auto end{ std::chrono::high_resolution_clock::now() };
+		const auto duration{ std::chrono::duration_cast<std::chrono::microseconds>( end - m_begin ).count() };
+
+		std::cout << "Duration: " << duration << " microseconds" << std::endl;
+	}
+
+private:
+
+	std::chrono::high_resolution_clock::time_point m_begin;
+};
+
+constexpr bool c_debug{ false };
 
 template<typename T>
 void vectorDebugPrint( const std::vector<T> &v )
@@ -19,38 +38,37 @@ void vectorDebugPrint( const std::vector<T> &v )
 	std::cout << std::endl;
 }
 
-void joyDebugPrint( const int64_t whoseTurn, const uint16_t weight, const int64_t baseJoy )
+void joyDebugPrint( const int32_t whoseTurn, const uint16_t weight, const int32_t baseJoy )
 {
 	if constexpr (not c_debug) return;
 
-	const std::string strWhoseTurn{ (whoseTurn == 1) ? "Vasya" : "Masha" };
+	const char *strWhoseTurn{ (whoseTurn == 1) ? "Vasya" : "Masha" };
 	std::cout << strWhoseTurn << " turn, weight: " << weight << ", base joy: " << baseJoy << std::endl;
 }
 
-auto readInput( const std::string_view path ) -> std::vector<uint16_t>
+auto readInput( const char *path ) -> std::vector<uint16_t>
 {
-	std::ifstream ifs( path.data() );
+	std::ifstream ifs( path );
 
 	size_t mushroomWeightCount{};
   ifs >> mushroomWeightCount;
 
 	std::vector<uint16_t> v_mushroomWeights;
-	v_mushroomWeights.reserve( mushroomWeightCount );
+	v_mushroomWeights.resize( mushroomWeightCount );
 
-	uint16_t mushroomWeight{};
-	while ( ifs >> mushroomWeight )
-	{
-		v_mushroomWeights.emplace_back( mushroomWeight );
-	}
+  for ( size_t i{ 0 }; i != mushroomWeightCount; ++i )
+  {
+	  ifs >> v_mushroomWeights[i];
+  }
 
 	vectorDebugPrint( v_mushroomWeights );
 
 	return v_mushroomWeights;
 }
 
-int64_t calcBaseJoy( const std::vector<uint16_t> &v_mushroomWeights )
+int32_t calcBaseJoy( const std::vector<uint16_t> &v_mushroomWeights )
 {
-	int64_t whoseTurn{ -1 }, baseJoy{};
+	int32_t whoseTurn{ -1 }, baseJoy{};
 	for ( const auto weight : v_mushroomWeights )
 	{
 		whoseTurn *= -1; //Если -1 то Машин
@@ -67,7 +85,7 @@ int64_t calcMaxJoy( const std::vector<uint16_t> &v_mushroomWeights )
 	uint16_t maxWeight{0};
 	uint16_t minWeight{ std::numeric_limits<uint16_t>::max() };
 
-	for ( size_t i{ 0 }; i != v_mushroomWeights.size(); i++ )
+	for ( uint32_t i{ 0 }; i != v_mushroomWeights.size(); i++ )
 	{
 		if ( i % 2 == 0)
 			minWeight = std::min( minWeight, v_mushroomWeights[i] );
@@ -76,8 +94,8 @@ int64_t calcMaxJoy( const std::vector<uint16_t> &v_mushroomWeights )
 			maxWeight = std::max( maxWeight, v_mushroomWeights[i] );
 	}
 
-	const int64_t baseJoy{ calcBaseJoy( v_mushroomWeights ) };
-	const int64_t maxJoy{ std::max( baseJoy, baseJoy + 2 * ( maxWeight - minWeight )) };
+	const int32_t baseJoy{ calcBaseJoy( v_mushroomWeights ) };
+	const int32_t maxJoy{ std::max( baseJoy, baseJoy + 2 * ( maxWeight - minWeight )) };
 
 	if constexpr ( c_debug )
 		std::cout << "Max weight: " << maxWeight << ", min weight: " << minWeight << ", max joy: " << maxJoy << std::endl;
@@ -87,6 +105,9 @@ int64_t calcMaxJoy( const std::vector<uint16_t> &v_mushroomWeights )
 
 int main()
 {
+	if constexpr ( c_debug )
+		Benchmark obj;
+
 	const auto maxJoy{ calcMaxJoy( readInput("input.txt") ) };
 
 	std::ofstream ofs("output.txt");
